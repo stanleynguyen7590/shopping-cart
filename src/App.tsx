56bd13f8f8868ad02f771182642d249c022c7619
Duy Anh Nguyen <stanley.nguyen7590@gmail.com>
2021-04-29 14:07:29 +0700

    Added styled compoents and global style to reset CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./App.css";
-
+import styled, { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+* {
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+`;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">Hello World!</div>
+    </>
+  );
 }
 
 export default App;
